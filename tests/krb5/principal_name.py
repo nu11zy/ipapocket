6e1ff343dbd0171a3b4389928bb079709b711caf
nu11zy <55335158+nu11zy@users.noreply.github.com>
2024-12-26 14:33:51 +0300

    init

---
+++ tests/krb5/principal_name.py
@@ -0,0 +1,57 @@
+import unittest
+from ipapocket.krb5.types import *
+from ipapocket.krb5.constants import *
+from ipapocket.exceptions.krb5 import *
+from ipapocket.krb5.constants.fields import *
+
+
+class TestPrincipalNameObject(unittest.TestCase):
+    def test_none(self):
+        pn = PrincipalName()
+        self.assertEqual(pn.name_type, None)
+        self.assertEqual(pn.name_value, KerberosStrings())
+
+    def test_type(self):
+        self.assertEqual(
+            PrincipalName(NameType.NT_PRINCIPAL).name_type,
+            NameType.NT_PRINCIPAL,
+        )
+
+    def test_value(self):
+        self.assertEqual(
+            PrincipalName(value=["admin", "ipa.test"]).name_value,
+            KerberosStrings(["admin", "ipa.test"]),
+        )
+        self.assertEqual(
+            PrincipalName(value="admin/ipa.test").name_value,
+            KerberosStrings(["admin", "ipa.test"]),
+        )
+        self.assertEqual(
+            PrincipalName(value="krbtgt/ipa.test").name_value,
+            ["krbtgt", "ipa.test"],
+        )
+        self.assertEqual(
+            PrincipalName(value="admin@ipa.test").name_value,
+            KerberosStrings("admin"),
+        )
+        self.assertEqual(
+            PrincipalName(value="krbtgt/ipa.test@ipa.test").name_value,
+            KerberosStrings(["krbtgt", "ipa.test"]),
+        )
+
+    def test_eq(self):
+        pn01 = PrincipalName(NameType.NT_SRV_HST, "IPA.TEST.LOCAL")
+        pn02 = PrincipalName(NameType.NT_PRINCIPAL, "admin@test.local")
+        pn03 = PrincipalName(NameType.NT_SRV_HST, "IPA.TEST.LOCAL")
+        self.assertNotEqual(pn01, pn02)
+        self.assertEqual(pn01, pn03)
+        self.assertNotEqual(pn02, pn03)
+
+    def test_asn1_native(self):
+        self.assertEqual(
+            dict(PrincipalName(NameType.NT_PRINCIPAL, "admin").to_asn1().native),
+            {
+                PRINCIPAL_NAME_NAME_TYPE: NameType.NT_PRINCIPAL.value,
+                PRINCIPAL_NAME_NAME_STRING: ["admin"],
+            },
+        )
---
+++ tests/krb5/realm.py
@@ -0,0 +1,27 @@
+import unittest
+from ipapocket.krb5.types import *
+from ipapocket.krb5.constants import *
+from ipapocket.exceptions.krb5 import *
+
+
+class TestRealmObject(unittest.TestCase):
+    def test_none(self):
+        self.assertEqual(Realm().realm, "")
+        self.assertEqual(Realm().realm.value, "")
+        self.assertEqual(Realm().realm, KerberosString())
+        self.assertEqual(Realm().realm.value, KerberosString().value)
+
+    def test_str(self):
+        self.assertEqual(Realm("IPA.TEST").realm, KerberosString("IPA.TEST"))
+
+    def test_kerberos_string(self):
+        self.assertEqual(Realm(KerberosString("IPA.TEST")), Realm("IPA.TEST"))
+        self.assertEqual(Realm(KerberosString()).realm, KerberosString())
+
+    def test_eq(self):
+        r01 = Realm("IPA01.TEST")
+        r02 = Realm("IPA02.TEST")
+        r03 = Realm("IPA01.TEST")
+        self.assertEqual(r01, r03)
+        self.assertNotEqual(r01, r02)
+        self.assertNotEqual(r02, r03)
